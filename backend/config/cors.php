7c788ef38e4652aedfedbe2d9103f01e40c208a0
Masamune0221 <dev.masamune02210@gmail.com>
2026-01-18 22:24:04 +0900

    Update allowed origins in CORS configuration

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,8 +6,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:3000',
+       'https://hitonichi-react.onrender.com',
     ],
 
     'allowed_origins_patterns' => [],

b99a168db768b71b25f51b15dd7507e6c26c522d
Masamune0221 <dev.masamune02210@gmail.com>
2026-01-19 00:54:55 +0900

    Update stateful domains for Sanctum configuration

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -16,12 +16,10 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,::1',
-        Str::startsWith(env('APP_URL', 'http://localhost'), 'https://') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
-        // Sanctum::currentRequestHost(),
-    ))),
+    'stateful' => explode(',', env(
+        'SANCTUM_STATEFUL_DOMAINS',
+        'hitonichi-react.onrender.com'
+    )),
 
     /*
     |--------------------------------------------------------------------------

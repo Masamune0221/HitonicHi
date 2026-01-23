6154bcade309e574f05850ffeeb8fe2ce2f54d82
Masamune0221 <dev.masamune02210@gmail.com>
2026-01-23 20:53:18 +0900

    Remove CSRF token validation exception for API

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
-        $middleware->validateCsrfTokens(except: ['/api/*']);
+        $middleware->validateCsrfTokens();
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {

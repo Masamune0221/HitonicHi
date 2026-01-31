3b7fec9727b78b63b701e90d4cba3bac5aefe83d
Masamune0221 <dev.masamune02210@gmail.com>
2026-02-01 01:48:50 +0900

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -96,6 +96,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'unix_socket' => env('DB_SOCKET', ''),
         ],
 
         'sqlsrv' => [

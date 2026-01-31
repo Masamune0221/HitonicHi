be6198ac7daf0c49f2e845ae9628a4b8847ea2a9
Masamune0221 <dev.masamune02210@gmail.com>
2026-02-01 04:12:35 +0900

    Update app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
-        $middleware->validateCsrfTokens();
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {

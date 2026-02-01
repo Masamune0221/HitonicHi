7205f50743b422849f1bd116366b10674c597acf
Masamune0221 <dev.masamune02210@gmail.com>
2026-02-01 22:28:05 +0900

    fix

--- backend/config/session.php
+++ backend/config/session.php
@@ -3,20 +3,40 @@
 use Illuminate\Support\Str;
 
 return [
-    'driver' => 'database',
-    'lifetime' => 120,
-    'expire_on_close' => false,
-    'encrypt' => false,
+
+    'driver' => env('SESSION_DRIVER', 'database'),
+
+    'lifetime' => (int) env('SESSION_LIFETIME', 120),
+
+    'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
+
+    'encrypt' => env('SESSION_ENCRYPT', false),
+
     'files' => storage_path('framework/sessions'),
-    'connection' => null,
-    'table' => 'sessions',
-    'store' => null,
+
+    'connection' => env('SESSION_CONNECTION'),
+
+    'table' => env('SESSION_TABLE', 'sessions'),
+
+    'store' => env('SESSION_STORE'),
+
     'lottery' => [2, 100],
-    'cookie' => 'hitonichi_session',
-    'path' => '/',
-    'domain' => null,
-    'secure' => true,
-    'http_only' => true,
-    'same_site' => 'none',
-    'partitioned' => true,
+
+    'cookie' => env(
+        'SESSION_COOKIE',
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+    ),
+
+    'path' => env('SESSION_PATH', '/'),
+
+    'domain' => env('SESSION_DOMAIN'),
+
+    'secure' => env('SESSION_SECURE_COOKIE',true),
+
+    'http_only' => env('SESSION_HTTP_ONLY', true),
+
+    'same_site' => env('SESSION_SAME_SITE', 'none'),
+
+    'partitioned' => env('SESSION_PARTITIONED_COOKIE', false),
+
 ];

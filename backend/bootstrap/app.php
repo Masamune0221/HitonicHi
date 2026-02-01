3b83b0ae47fc731f3f31ba93783afe4fc7390c95
Masamune0221 <dev.masamune02210@gmail.com>
2026-02-01 18:38:24 +0900

    2026/2/1 Masamune fix

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,9 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
         $middleware->trustProxies(at: '*');
-        $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

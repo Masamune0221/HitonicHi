050e0844b19b3e364010db1556cfea6cdb6c6d13
Masamune0221 <dev.masamune02210@gmail.com>
2026-02-01 18:31:05 +0900

    Update app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,9 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
         $middleware->trustProxies(at: '*');
-        $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

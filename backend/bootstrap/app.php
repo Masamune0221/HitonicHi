afaf23748f858816e07aeb35dff6aa98a76a2098
Masamune0221 <dev.masamune02210@gmail.com>
2026-02-01 21:54:26 +0900

    Update app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Cookie;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,39 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
-
-        $middleware->encryptCookies(except: [
-            'XSRF-TOKEN',
-        ]);
-
         $middleware->trustProxies(at: '*');
-
-        // Cookie属性を強制上書きするミドルウェア
-        $middleware->append(function (Request $request, $next) {
-            $response = $next($request);
-            
-            // レスポンスが正しい型（ヘッダー操作可能）か確認
-            if (method_exists($response, 'header')) {
-                $cookies = $response->headers->getCookies();
-                foreach ($cookies as $cookie) {
-                    $response->headers->setCookie(
-                        new Cookie(
-                            $cookie->getName(),
-                            $cookie->getValue(),
-                            $cookie->getExpiresTime(),
-                            $cookie->getPath(),
-                            $cookie->getDomain(),
-                            true, // secure
-                            $cookie->isHttpOnly(),
-                            $cookie->isRaw(),
-                            'none', // samesite
-                            $cookie->isPartitioned()
-                        )
-                    );
-                }
-            }
-            return $response;
-        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

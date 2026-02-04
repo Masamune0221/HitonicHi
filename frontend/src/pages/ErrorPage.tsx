92f7340465b9d4e7377effd8b82c69a975760f3e
Masamune0221 <dev.masamune02210@gmail.com>
2026-02-04 21:14:10 +0900

    2026/2/4 Masamune エラー画面のレスポンシブ対応

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -3,7 +3,7 @@ import ContentCard from '../components/layout/ContentCard';
 export default function Error(){
     return (
          <BubbleBackground className="w-screen h-screen">
-            <div className='flex flex-col justify-center items-center h-full w-1/2 mx-auto'>
+            <div className='w-2/3 flex flex-col justify-center items-center h-full mx-auto'>
             <ContentCard className=' flex w-full h-1/2 text-center justify-center items-center'>
             <div className="text-center">
                 <h2 className="text-3xl font-bold text-white tracking-wide">
